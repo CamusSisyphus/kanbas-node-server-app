09e8785b3dd39785a04ca27be2c6660f5ecbf161
Your NameCamusSisyphus <masonwu624@gmail.com>
2023-12-03 18:23:44 -0500

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -4,15 +4,38 @@ import HelloRoutes from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
-
+import mongoose from "mongoose";
+import session from "express-session";
+import "dotenv/config";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(express.json());
 app.use(
-    cors()
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+ 
   );
+  const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
 
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
