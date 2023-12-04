b794ba4b7ae27ae804dafbd04aea47fdce8d25b4
Your NameCamusSisyphus <masonwu624@gmail.com>
2023-12-04 16:27:52 -0500

    amend app.js

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ import mongoose from "mongoose";
 import session from "express-session";
 import "dotenv/config";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
-console.log(process.env.FRONTEND_URL)  
+
 app.use(
   cors({
     credentials: true,
@@ -23,17 +23,17 @@ app.use(
   })
  
   );
-  const sessionOptions = {
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
   };
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-    };
   }
 
 app.use(session(sessionOptions));
